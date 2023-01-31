961ea013fd379cb68b5803e018c300f76410eebc
Andrey <an.nikishin2012@yandex.ru>
2023-01-31 15:21:30 +0300

    Дз решено

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+
+void Printnumber(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + (numberM++);
+    Printnumber(numberM, numberN, sum);
+}
+
+
+Console.WriteLine("Введите число m ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Printnumber(numberM, numberN, sum);
---
+++ task68/Program.cs
@@ -0,0 +1,29 @@
+﻿// // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+// // m = 2, n = 3 -> A(m,n) = 9
+// // m = 3, n = 2 -> A(m,n) = 29
+
+int number(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return number(m - 1, 1);
+    }
+    else
+    {
+        return (number(m - 1, number(m, n - 1)));
+    }
+}
+
+
+Console.WriteLine("Введите число m ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(number(numberM, numberN));
